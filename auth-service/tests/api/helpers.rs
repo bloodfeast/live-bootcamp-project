a2c52bb0ad40e51a71a5a1f84c6daf25308765fd
Brandon White <bwhite@mdlogic.com>
2025-01-10 22:46:06 -0500

    [Sprint 1][Task 7] - impl signup

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -1,6 +1,12 @@
+use uuid::Uuid;
+
 pub struct TestApp {
     pub address: String,
     pub http_client: reqwest::Client,
+}
+
+pub fn get_random_email() -> String {
+    format!("{}@example.com", Uuid::new_v4())
 }
 
 impl TestApp {
@@ -36,51 +42,61 @@ impl TestApp {
             .expect("Failed to send request")
     }
 
-    pub async fn post_signup(&self, body: &str) -> reqwest::Response {
+    pub async fn post_signup<T>(&self, body: &T) -> reqwest::Response
+    where T: serde::Serialize + ?Sized
+    {
         self.http_client
             .post(&format!("{}/signup", &self.address))
             .header("content-type", "application/json")
-            .body(body.to_string())
+            .json(&body)
             .send()
             .await
             .expect("Failed to send request")
     }
 
-    pub async fn post_login(&self, body: &str) -> reqwest::Response {
+    pub async fn post_login<T>(&self, body: &T) -> reqwest::Response
+    where T: serde::Serialize + ?Sized
+    {
         self.http_client
             .post(&format!("{}/login", &self.address))
             .header("content-type", "application/json")
-            .body(body.to_string())
+            .json(&body)
             .send()
             .await
             .expect("Failed to send request")
     }
 
-    pub async fn post_logout(&self, body: &str) -> reqwest::Response {
+    pub async fn post_logout<T>(&self, body: &T) -> reqwest::Response
+    where T: serde::Serialize + ?Sized
+    {
         self.http_client
             .post(&format!("{}/logout", &self.address))
             .header("content-type", "application/json")
-            .body(body.to_string())
+            .json(&body)
             .send()
             .await
             .expect("Failed to send request")
     }
 
-    pub async fn post_verify_2fa(&self, body: &str) -> reqwest::Response {
+    pub async fn post_verify_2fa<T>(&self, body: &T) -> reqwest::Response
+    where T: serde::Serialize + ?Sized
+    {
         self.http_client
             .post(&format!("{}/verify-2fa", &self.address))
             .header("content-type", "application/json")
-            .body(body.to_string())
+            .json(&body)
             .send()
             .await
             .expect("Failed to send request")
     }
 
-    pub async fn post_verify_token(&self, body: &str) -> reqwest::Response {
+    pub async fn post_verify_token<T>(&self, body: &T) -> reqwest::Response
+    where T: serde::Serialize + ?Sized
+    {
         self.http_client
             .post(&format!("{}/verify-token", &self.address))
             .header("content-type", "application/json")
-            .body(body.to_string())
+            .json(&body)
             .send()
             .await
             .expect("Failed to send request")
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,59 @@
-use crate::helpers::TestApp;
-
+use crate::helpers::{get_random_email, TestApp};
 #[tokio::test]
-async fn signup_returns_200() {
+async fn should_return_200_for_happy_path() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup(r#"{"email": "[email protected]", "password": "password"}"#).await;
+    let test_cases = [
+        serde_json::json!({
+            "email": "some_email@some_domain.com",
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": "some.email@some.domain.com",
+            "password": "password123",
+            "requires2FA": true
+        }),
+    ];
 
-    assert_eq!(response.status().as_u16(), 200);
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            200,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
+#[tokio::test]
+async fn should_return_422_if_malformed_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email(); // Call helper method to generate email
+
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "password123"
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
