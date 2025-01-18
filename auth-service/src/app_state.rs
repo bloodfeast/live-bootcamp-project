d20c700a262a951c3fcc0336946b025b1a500592
Brandon White <bwhite@mdlogic.com>
2025-01-17 22:56:37 -0500

    [Sprint 2][final] - added a little more documentation

--- auth-service/src/app_state.rs
+++ auth-service/src/app_state.rs
@@ -5,14 +5,16 @@ use crate::domain::UserStore;
 /// The `AppState` struct holds the application state.
 /// It contains a reference to the user store.
 ///
-/// The `AppState` struct is generic over the type `T`, which must implement the `UserStore` trait.
-/// I opted to use a generic implementation here instead of a trait object because it allows us to
-/// specify the concrete type of the user store at compile time.
+/// The `AppState` struct is generic over the type `T`, which must implement the `UserStore` trait. \
+/// *I opted to use a generic implementation here instead of a trait object because it allows us to
+/// specify the concrete type of the user store at compile time.*
 ///
-/// - This implementation also adds a `Clone` bound to the `T` type parameter. \
+/// - This implementation also adds a `Clone` bound to the `T` type parameter which allows us to
+/// wrap the `UserStore` in an `Arc` smart pointer with a `RwLock` to allow for concurrent access.\
 /// \
 /// This is in addition to the `UserStore` trait bound. \
 /// which already implements `Sized`, `Send`, and `Sync` \
+/// \
 /// **see also: [domain/data_stores.rs](crate::domain::data_stores::UserStore)**
 ///
 /// ###### Pros:
@@ -21,7 +23,9 @@ use crate::domain::UserStore;
 ///
 /// ###### Cons:
 /// - It requires more boilerplate code, which could be a bit cumbersome later
-/// if we have a lot of different types that implement the `UserStore` trait.
+/// if we have a lot of different types that implement the `UserStore` trait with different
+/// trait bound requirements. \
+/// **see: [Application::build](crate::Application::build)**
 ///
 #[derive(Clone)]
 pub struct AppState<T: UserStore> {
