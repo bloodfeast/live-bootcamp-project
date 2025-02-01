0d1123207e2bdcc818781f3705a562ff3b8d9fea
Brandon White <bwhite@mdlogic.com>
2025-02-01 12:24:15 -0500

    [Sprint 4][cleanup] - missed change in commit

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -27,17 +27,16 @@ where T: UserStore,
     let login_attempt_id = LoginAttemptId::from_str(request.login_attempt_id.as_str())
         .map_err(|_| AuthAPIError::MalformedRequest)?;
 
-
     let two_fac_code = TwoFACode::from_str(request.two_fac_code.as_str())
-        .map_err(|_| AuthAPIError::MissingToken)?;
+        .map_err(|_| AuthAPIError::MalformedRequest)?;
 
     let mut two_fac_code_store = state.two_fa_code_store.write().await;
     let stored_code_tuple = two_fac_code_store.get_code(&email).await
         .map_err(|_| AuthAPIError::InvalidCredentials);
 
     return match stored_code_tuple {
-        Ok(t) => {
-            if t.0 == login_attempt_id && t.1 == two_fac_code {
+        Ok((logon_attempt, tfa_code)) => {
+            if logon_attempt == login_attempt_id && tfa_code == two_fac_code {
                 // Remove the code from the store, so it can't be used again.
                 two_fac_code_store.remove_code(&email).await
                     .map_err(|_| AuthAPIError::UnexpectedError)?;
